49d12be15cd1d647d8906fa4b7a0fcb306e84126
williamsartijose <williamsartijose@hotmail.com>
2022-01-12 11:26:27 -0300

    MovieStars

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,13 @@
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 function Listing(){
 
     return(
+        <>
         <Pagination />
+        <MovieStars />
+        </>
     );
 }
 
