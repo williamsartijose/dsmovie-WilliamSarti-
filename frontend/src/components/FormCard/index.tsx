bfb186b4a20547c11bd5560da64a92e8d0ad4c40
williamsartijose <williamsartijose@hotmail.com>
2022-01-15 16:09:28 -0300

    Save - UseNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
-import { BASE_URL } from "utils/request";
+import { BASE_URL } from "utils/requests";
 import { validateEmail } from "utils/validate";
 import "./styles.css";
 
@@ -44,7 +44,7 @@ function FormCard({ movieId }: Props) {
     }
 
     axios(config).then(response => {
-      //navigate("/");
+      navigate("/");
     });
     
   };
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,7 +3,7 @@ import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { MoviePage } from "types/movie";
-import { BASE_URL } from "utils/request";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
